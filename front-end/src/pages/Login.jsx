c867efe36fd566e247dcc1474b461c1ecae3aa2c
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2025-04-06 09:10:09 -0300

    feat: implementando componentes e estilo na tela de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-
-
+import '../style/Login.css';
+import { Button } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import iconLogin from '../assets/login/user.png';
+import Col from 'react-bootstrap/Col';
 
 function Login() {
     return (
-       <>
-          <h1>Login</h1>
-       </>
-    )
+      <>
+        <div className="Form">
+          <form className="container-inputs">
+            <Image className="icon-login-img" src={ iconLogin} roundedCircle />   
+            
+            <input type="email" placeholder="E-mail" name="" />
+
+            <input type="password" placeholder="Senha" name="" />
+
+            <Button variant="outline-light">Enviar</Button>
+          </form>
+        </div>
+      </>
+    );
 }
 
 
